a3e6ce9bf781ccab6b2022793441353b27e80b98
takemeto404 <takemeto404@gmail.com>
2023-04-29 15:43:43 +0300

    Simplified Y positioning depending on height, 80% of highFrequency logging, GenerateRunConfigs

--- Assets/Scripts/SessionManagement/ExperimentManager.cs
+++ Assets/Scripts/SessionManagement/ExperimentManager.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using Logging;
+using SpatialUIPlacement;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -20,7 +22,7 @@ public partial class ExperimentManager: MonoBehaviour
         
         // time
         "HumanReadableTimestampUTC", // absolute time of the selection
-        "SystemClockTimestampMs", // time passed from activating the first target
+        "SystemClockTimestampMs", // time passed from selecting the first target
         
         // selection
         "ActiveTargetIndex",
@@ -47,7 +49,7 @@ public partial class ExperimentManager: MonoBehaviour
         
         // time
         "HumanReadableTimestampUTC", // absolute time of the selection
-        "SystemClockTimestampMs", // time passed from activating the first target
+        "SystemClockTimestampMs", // time passed from start (activating the first target)
         
         // track transform
         "TrackPositionX",
@@ -65,19 +67,133 @@ public partial class ExperimentManager: MonoBehaviour
         "TrackQuaternionW",
         
         // WalkingDirectionTransform
-        "TrackPositionX",
-        "TrackPositionY",
-        "TrackPositionZ",
-        "TrackForwardX",
-        "TrackForwardY",
-        "TrackForwardZ",
-        "TrackUpX",
-        "TrackUpY",
-        "TrackUpZ",
-        "TrackQuaternionX",
-        "TrackQuaternionY",
-        "TrackQuaternionZ",
-        "TrackQuaternionW",
+        "WalkingDirectionPositionX",
+        "WalkingDirectionPositionY",
+        "WalkingDirectionPositionZ",
+        "WalkingDirectionForwardX",
+        "WalkingDirectionForwardY",
+        "WalkingDirectionForwardZ",
+        "WalkingDirectionUpX",
+        "WalkingDirectionUpY",
+        "WalkingDirectionUpZ",
+        "WalkingDirectionQuaternionX",
+        "WalkingDirectionQuaternionY",
+        "WalkingDirectionQuaternionZ",
+        "WalkingDirectionQuaternionW",
+        
+        // head (CenterEyeAnchor)
+        "HeadPositionX",
+        "HeadPositionY",
+        "HeadPositionZ",
+        "HeadForwardX",
+        "HeadForwardY",
+        "HeadForwardZ",
+        "HeadUpX",
+        "HeadUpY",
+        "HeadUpZ",
+        "HeadQuaternionX",
+        "HeadQuaternionY",
+        "HeadQuaternionZ",
+        "HeadQuaternionW",
+        
+        // neck model
+        "NeckBasePositionX",
+        "NeckBasePositionY",
+        "NeckBasePositionZ",
+        "NeckBaseForwardX",
+        "NeckBaseForwardY",
+        "NeckBaseForwardZ",
+        "NeckBaseUpX",
+        "NeckBaseUpY",
+        "NeckBaseUpZ",
+        "NeckBaseQuaternionX",
+        "NeckBaseQuaternionY",
+        "NeckBaseQuaternionZ",
+        "NeckBaseQuaternionW",
+
+        // dominant hand palmCenterAnchor
+        "DominantPalmCenterPositionX",
+        "DominantPalmCenterPositionY",
+        "DominantPalmCenterPositionZ",
+        "DominantPalmCenterForwardX",
+        "DominantPalmCenterForwardY",
+        "DominantPalmCenterForwardZ",
+        "DominantPalmCenterUpX",
+        "DominantPalmCenterUpY",
+        "DominantPalmCenterUpZ",
+        "DominantPalmCenterQuaternionX",
+        "DominantPalmCenterQuaternionY",
+        "DominantPalmCenterQuaternionZ",
+        "DominantPalmCenterQuaternionW",
+        
+        // dominant hand index finger tip
+        "DominantIndexTipPositionX",
+        "DominantIndexTipPositionY",
+        "DominantIndexTipPositionZ",
+        "DominantIndexTipForwardX",
+        "DominantIndexTipForwardY",
+        "DominantIndexTipForwardZ",
+        "DominantIndexTipUpX",
+        "DominantIndexTipUpY",
+        "DominantIndexTipUpZ",
+        "DominantIndexTipQuaternionX",
+        "DominantIndexTipQuaternionY",
+        "DominantIndexTipQuaternionZ",
+        "DominantIndexTipQuaternionW",
+        
+        // weak hand palmCenterAnchor
+        "WeakPalmCenterPositionX",
+        "WeakPalmCenterPositionY",
+        "WeakPalmCenterPositionZ",
+        "WeakPalmCenterForwardX",
+        "WeakPalmCenterForwardY",
+        "WeakPalmCenterForwardZ",
+        "WeakPalmCenterUpX",
+        "WeakPalmCenterUpY",
+        "WeakPalmCenterUpZ",
+        "WeakPalmCenterQuaternionX",
+        "WeakPalmCenterQuaternionY",
+        "WeakPalmCenterQuaternionZ",
+        "WeakPalmCenterQuaternionW",
+        
+        // all targets (center of targets circle)
+        "AllTargetsPositionX",
+        "AllTargetsPositionY",
+        "AllTargetsPositionZ",
+        "AllTargetsForwardX",
+        "AllTargetsForwardY",
+        "AllTargetsForwardZ",
+        "AllTargetsUpX",
+        "AllTargetsUpY",
+        "AllTargetsUpZ",
+        "AllTargetsQuaternionX",
+        "AllTargetsQuaternionY",
+        "AllTargetsQuaternionZ",
+        "AllTargetsQuaternionW",
+        
+        // active target
+        "ActiveTargetPositionX",
+        "ActiveTargetPositionY",
+        "ActiveTargetPositionZ",
+        "ActiveTargetForwardX",
+        "ActiveTargetForwardY",
+        "ActiveTargetForwardZ",
+        "ActiveTargetUpX",
+        "ActiveTargetUpY",
+        "ActiveTargetUpZ",
+        "ActiveTargetQuaternionX",
+        "ActiveTargetQuaternionY",
+        "ActiveTargetQuaternionZ",
+        "ActiveTargetQuaternionW",
+        
+        // 2d coordinates
+        "SelectorProjectionOntoAllTargetsX",
+        "SelectorProjectionOntoAllTargetsY",
+        "ActiveTargetIndex",
+        "ActiveTargetInsideAllTargetsX",
+        "ActiveTargetInsideAllTargetsY",
+        "IsSelectorInsideCollider", // 0 or 1, where 1 means yes he is inside
+        "DistanceFromSelectorToAllTargetsOXYPlane", // maybe negative (if he is inside). Note, that collider is not infinite, just about 20-25cm. If outside it, always positive distance
     };
     
     // not used for trainings. To finish training, method (OnServerSaidFinishTraining should be called)
@@ -86,18 +202,31 @@ public partial class ExperimentManager: MonoBehaviour
     private State _state;
     private RunConfig _runConfig; 
     
+    [SerializeField] private TargetsManager targetsManager;
+    
     [SerializeField] private GameObject metronome;
     [SerializeField] private GameObject errorIndicator;
     [SerializeField] private GameObject track;
     [SerializeField] private GameObject sceneLight; // remark: we interpret it as track in standing context. Hand Ref and path ref depend on it, actually
-    
+
+    [SerializeField] private GameObject walkingDirection; // walking context (relative to track)
+    [SerializeField] private GameObject standingDirection; // standing context (relative to light)
+
+    [SerializeField] private GameObject headset;
+    [SerializeField] private GameObject neckBase;
+    // SimplifiedComfortUIPlacement position.y depends on headset.
+    // We refresh it when we want (usually after "prepare" command from server) by calling comfortUICoordinateY.Refresh()
+    // Path-refFrame positioning depend on it.
+    [SerializeField] private SimplifiedComfortUIPlacement comfortUICoordinateY;
+
     [Space] // oculus hands here. Note, that we keep inactive gameObjects which we don't use 
     [SerializeField] private GameObject leftIndexTip;
     [SerializeField] private GameObject rightIndexTip;
     [SerializeField] private GameObject leftPalmCenter;
     [SerializeField] private GameObject rightPalmCenter;
-    private GameObject dominantHandIndexTip;
-    private GameObject weakHandPalmCenter;
+    private GameObject dominantHandIndexTip; // holds selector
+    private GameObject weakHandPalmCenter; // holds target
+    private GameObject dominantHandPalmCenter; // just for logging
 
     [Space] // reference frames go here. Note, that we keep inactive gameObjects which we don't use
     // left-right for palmRef
@@ -117,6 +246,84 @@ public partial class ExperimentManager: MonoBehaviour
     private GameObject[] inactiveRefFrames;
 
     public bool IsRunning { get; private set; }
+
+    private bool loggingIsOn;
+    private AsyncHighFrequencyCSVLogger _selectionsLogger;
+    private AsyncHighFrequencyCSVLogger _highFrequencyLogger;
+
+    private void Start()
+    {
+        
+    }
+
+    private void LateUpdate()
+    {
+        // asyncHighFrequencyLogging stuff goes here
+
+        if (!loggingIsOn) return;
+        // now we assume that we are inside trial session and logger is initialized already
+
+        var row = _highFrequencyLogger.CurrentRow;
+        
+        // TODO: add Ids, Conditions, Times
+
+        var trackTransform = _runConfig.context == Context.Walking ? track.transform : sceneLight.transform;
+        LogObjectTransform("Track", trackTransform);
+        
+        var walkingDirectionTransform = _runConfig.context == Context.Walking ? walkingDirection.transform : standingDirection.transform;
+        LogObjectTransform("WalkingDirection", walkingDirectionTransform);
+        
+        LogObjectTransform("Head", headset.transform); // center eye anchor
+        LogObjectTransform("NeckBase", neckBase.transform);
+
+        LogObjectTransform("DominantPalmCenter", dominantHandPalmCenter.transform);
+        LogObjectTransform("DominantIndexTip", dominantHandIndexTip.transform); // holds selector
+        LogObjectTransform("WeakPalmCenter", weakHandPalmCenter.transform); // holds targets
+
+        var allTargets = targetsManager.transform;
+        var activeTarget = targetsManager.ActiveTarget.target.transform;
+        LogObjectTransform("AllTargets", allTargets);
+        LogObjectTransform("ActiveTarget", activeTarget);
+
+        var selectorPosition = dominantHandIndexTip.transform.position;
+        var selectorProjection = Utils.Math.ProjectPointOntoOXYPlane(allTargets, selectorPosition);
+        row.SetColumnValue("SelectorProjectionOntoAllTargetsX", selectorProjection.local.x);
+        row.SetColumnValue("SelectorProjectionOntoAllTargetsY", selectorProjection.local.y);
+        var isInside = targetsManager.IsSelectorInsideCollider;
+        row.SetColumnValue("IsSelectorInsideCollider", isInside ? 1 : 0);
+        var distance = (selectorPosition - selectorProjection.world).magnitude;
+        var distanceToLog = isInside ? -distance : distance;
+        row.SetColumnValue("DistanceFromSelectorToAllTargetsOXYPlane", distanceToLog);
+        
+        row.SetColumnValue("ActiveTargetIndex", targetsManager.ActiveTarget.targetIndex);
+        var activeTargetProjection = Utils.Math.ProjectPointOntoOXYPlane(allTargets, activeTarget.position);
+        row.SetColumnValue("ActiveTargetInsideAllTargetsX", activeTargetProjection.local.x);
+        row.SetColumnValue("ActiveTargetInsideAllTargetsY", activeTargetProjection.local.y);
+    }
+
+    private void LogObjectTransform(string objectPrefix, Transform objectTransform)
+    {
+        var row = _highFrequencyLogger.CurrentRow;
+
+        var position = objectTransform.position;
+        var forward = objectTransform.forward;
+        var up = objectTransform.up;
+        var quaternion = objectTransform.rotation;
+
+        row.SetColumnValue(objectPrefix + "PositionX", position.x);
+        row.SetColumnValue(objectPrefix + "PositionY", position.y);
+        row.SetColumnValue(objectPrefix + "PositionZ", position.z);
+        row.SetColumnValue(objectPrefix + "ForwardX", forward.x);
+        row.SetColumnValue(objectPrefix + "ForwardY", forward.y);
+        row.SetColumnValue(objectPrefix + "ForwardZ", forward.z);
+        row.SetColumnValue(objectPrefix + "UpX", up.x);
+        row.SetColumnValue(objectPrefix + "UpY", up.y);
+        row.SetColumnValue(objectPrefix + "UpZ", up.z);
+        row.SetColumnValue(objectPrefix + "QuaternionX", quaternion.x);
+        row.SetColumnValue(objectPrefix + "QuaternionY", quaternion.y);
+        row.SetColumnValue(objectPrefix + "QuaternionZ", quaternion.z);
+        row.SetColumnValue(objectPrefix + "QuaternionW", quaternion.w);
+    }
 
     private void ActualizeReferenceFrames()
     {
@@ -173,9 +380,10 @@ public partial class ExperimentManager: MonoBehaviour
         {
             leftIndexTip.SetActive(true);
             rightIndexTip.SetActive(false);
-            leftPalmCenter.SetActive(false);
+            leftPalmCenter.SetActive(true);
             rightPalmCenter.SetActive(true);
 
+            dominantHandPalmCenter = leftPalmCenter;
             dominantHandIndexTip = leftIndexTip;
             weakHandPalmCenter = rightPalmCenter;
         }
@@ -184,10 +392,11 @@ public partial class ExperimentManager: MonoBehaviour
             leftIndexTip.SetActive(false);
             rightIndexTip.SetActive(true);
             leftPalmCenter.SetActive(true);
-            rightPalmCenter.SetActive(false);
+            rightPalmCenter.SetActive(true);
             
             dominantHandIndexTip = rightIndexTip;
             weakHandPalmCenter = leftPalmCenter;
+            dominantHandPalmCenter = rightPalmCenter;
         }
     }
     
@@ -263,6 +472,16 @@ partial class ExperimentManager
         public readonly bool isTraining;
         public readonly Context context;
         public readonly ReferenceFrame referenceFrame;
+
+        public RunConfig(int participantID, bool leftHanded, bool isMetronomeTraining, bool isTraining, Context context, ReferenceFrame referenceFrame)
+        {
+            this.participantID = participantID;
+            this.leftHanded = leftHanded;
+            this.isMetronomeTraining = isMetronomeTraining;
+            this.isTraining = isTraining;
+            this.context = context;
+            this.referenceFrame = referenceFrame;
+        }
     }
     
     private enum State
--- Assets/Scripts/SessionManagement/MainProcess/HelmetMainProcess.cs
+++ Assets/Scripts/SessionManagement/MainProcess/HelmetMainProcess.cs
@@ -1,17 +1,80 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class HelmetMainProcess: ExperimentNetworkClient
 {
     [SerializeField] private StandingTrainingManager standingTrainingManager;
 
-    protected sealed override void Start()
+    private int participantId;
+    private bool leftHanded;
+
+    private ExperimentManager.RunConfig[] _runConfigs;
+    private int currentRunIndex;
+    private RunStage currentRunStage;
+    
+    [Serializable]
+    public enum RunStage
+    {
+        Idle, // "prepare" command was not given by server yet. ExperimentManager is in Idle state, actually
+        Preparing, // yellow flag. "prepare" was given. Track(and light) were positioned, targets were shown, but not selectable yet
+        Running // ExperimentManager is running training/trial. If training, then stop will be called by server command. Otherwise, when participant will finish selecting all targets
+    }
+
+    public static ExperimentManager.RunConfig[] GenerateRunConfigs(int participantId, bool leftHanded)
+    {
+        var generateNotTrainingRunConfig =
+            new Func<ExperimentManager.ReferenceFrame, ExperimentManager.Context, ExperimentManager.RunConfig>
+                ((rf, ctx) => new ExperimentManager.RunConfig(participantId, leftHanded, false, false, ctx, rf));
+
+        var result = new List<ExperimentManager.RunConfig>(15); // 7 (rf*context) + 7 trainings + 1 metronome training
+
+        var latinSquaredNotTrainings = Utils.Math.balancedLatinSquare(new ExperimentManager.RunConfig[]
+        {
+            generateNotTrainingRunConfig(ExperimentManager.ReferenceFrame.PalmReferenced, ExperimentManager.Context.Standing),
+            generateNotTrainingRunConfig(ExperimentManager.ReferenceFrame.HandReferenced, ExperimentManager.Context.Standing),
+            generateNotTrainingRunConfig(ExperimentManager.ReferenceFrame.PathReferenced, ExperimentManager.Context.Standing),
+            generateNotTrainingRunConfig(ExperimentManager.ReferenceFrame.PalmReferenced, ExperimentManager.Context.Walking),
+            generateNotTrainingRunConfig(ExperimentManager.ReferenceFrame.HandReferenced, ExperimentManager.Context.Walking),
+            generateNotTrainingRunConfig(ExperimentManager.ReferenceFrame.PathReferenced, ExperimentManager.Context.Walking),
+            generateNotTrainingRunConfig(ExperimentManager.ReferenceFrame.PathReferencedNeck, ExperimentManager.Context.Walking),
+        }, participantId);
+
+        var turnIntoTraining = new Func<ExperimentManager.RunConfig, ExperimentManager.RunConfig>
+            (config => new ExperimentManager.RunConfig(config.participantID, config.leftHanded, false, true,
+                config.context, config.referenceFrame));
+
+        // adding training and not trainings with reference frames
+        int added = 0;
+        foreach (var notTrainingConfig in latinSquaredNotTrainings)
+        {
+            result.Insert(added, turnIntoTraining(notTrainingConfig));
+            result.Insert(added + 1, notTrainingConfig);
+            added += 2;
+        }
+        
+        // adding training to go with metronome
+        int index = 0;
+        foreach (var runConfig in result)
+        {
+            if (runConfig.context == ExperimentManager.Context.Walking)
+                break;
+            index++;
+        }
+        result.Insert(index, new ExperimentManager.RunConfig(participantId, leftHanded, 
+            true, // indicates this is training with metronome
+            // now below parameters don't matter
+            true,
+            ExperimentManager.Context.Walking,
+            ExperimentManager.ReferenceFrame.PalmReferenced
+            ));
+
+        return result.ToArray();
+    }
+    
+    protected override void Start()
     {
         base.Start();
-        
-        standingTrainingManager.selectionDone.AddListener((success, targetIndex) =>
-        {
-            Send(new MessageFromHelmet.SelectionDone(success, targetIndex));
-        });
     }
 
     protected override void Receive(MessageToHelmet message)
